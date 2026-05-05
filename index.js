31cc68b9ae8df0e43c2c4839e3e23d4ec53e8545
Ana Rodríguez <anarodriguezpiqueras@gmail.com>
2026-05-05 21:31:40 +0200

    Actualizacion por audio

--- index.js
+++ index.js
@@ -68,6 +68,9 @@ function handleTimeout() {
       fade: false,
     });
 
+    setInterval(() => {
+      document.body.style.backgroundColor = '#' + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0');
+    }, 250);
 
     const cancion = new Audio("parchis.mp3");
     cancion.play();
